2eeb1aced05f07a35cb5b2c3c092eaf5daeae2bb
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-03-23 19:57:53 +1000

    How many chacters and special symbols added

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+ 
+
+function generatePassword(howCharacters, speCharacter) {
+  var passwordLength = 129;
+  var speCharacter = "!'#()*+,-.%/:;<=>?@&[\]$^_`{|}~";
+  var password = "";
+  var text;
+
+  // Ask user how many character wants in the password.
+  var howCharacters = window.prompt("How many characters would you like your password to contain? ")
+
+
+if (!howCharacters) {
+  return alert("You Must choose at least one character type.");
+
+} else if (passwordLength < 8) {
+  return alert("Password lenght must be at least 8 characters." );
+} else {
+  var speCharacter = window.prompt("Click ok to confirm including special characters.")
+}
+
+var password = Math.floor(Math.random() * passwordLength);
+
+if (howCharacters === passwordLength) {
+
+
+}
+
+  return ""
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
