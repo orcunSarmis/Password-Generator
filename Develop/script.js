97f35062132d30e105cac0d8960adb7b5641f57f
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-03-24 22:29:18 +1000

    New prompt method

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,68 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
  
 function generatePassword() {
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var symbol =  " !\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~";
   var numbers = "1234567890";
-  var passwordLength = 129;
+  var howCharacters;
+  var passwordLenght;
+  var specialChar;
+
+  var maxLength = 8;
+  var maxLength = 129;
   var password = "";
   var text;
 
   // Ask user how many character wants in the password.
-  // var howCharacters = window.prompt("How many characters would you like your password to contain? ")
+   howCharacters = prompt("How many characters would you like your password to contain? ",  "Min:8-Max:129")
 
-  var passwordLength = window.prompt("How many characters would you like your password to contain? ")
-
-if (!passwordLength) {
+   specialChar = prompt("Click OK to confirm including special chacters.", " !\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~")
+  console.log(howCharacters);
   
-  return alert("You must choose at least one character type.");
-} if (passwordLength < 8) {
 
-  return alert("Password lenght must be at least 8 characters." );
-} if (passwordLength > 129) {
-  return alert("Password lenght must less than 129 characters.");
-} else 
 
-var specialCh = window.confirm("Click OK to confirm including special chacters.")
-if (specialCh == true) {
-  return ; 
-} if (specialCh == false) {
-  return alert("You must choose at least one character type.");
-}
+//   var maxLength = window.prompt("How many characters would you like your password to contain? ")
 
-var low = window.confirm("Click OK to confirm including lowercase chacters.")
-// if ()
+// if (!maxLength) {
+  
+//   return alert("You must choose at least one character type.");
+// } if (maxLength < 8) {
 
-var upper = window.confirm("Click OK to confirm including uppercase chacters.")
+//   return alert("Password lenght must be at least 8 characters." );
+// } if (maxLength > 129) {
+//   return alert("Password lenght must less than 129 characters.");
+// } else 
+
+// var specialCh = window.confirm("Click OK to confirm including special chacters.")
+// if (specialCh == true) {
+//   return ; 
+// } if (specialCh == false) {
+
+//   return alert("You must choose at least one character type."); 
+// }
+
+// var low = window.confirm("Click OK to confirm including lowercase chacters.")
+// if (low == true) {
+// return;
+// }
+
+// var upper = window.confirm("Click OK to confirm including uppercase chacters.")
 
 
 
@@ -50,14 +76,3 @@ var upper = window.confirm("Click OK to confirm including uppercase chacters.")
   return ""
 }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
