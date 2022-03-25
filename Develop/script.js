4b1072c97227a6c1958045302ee7689e68dc72de
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-03-25 20:19:44 +1000

    Code clean up

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,91 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","H","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var symbol =  [" ", "!", "'", "#", "$", "%", "&", "*", "(", ")", "?"];
+var numbers = [1,2,3,4,5,6,7,8,9,0];
+
+var howCharacters;
+
+var passwordLenght;
+var specialChar;
+var numeric;
+var lowC;
+var upperC;
+var possibleChar = "";
+var randomChar = "";
+
+var maxLength = 8;
+var maxLength = 128;
+
+var text;  
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+ 
+function generatePassword() {
+  var password = "";
+   // Ask user how many character wants in the password.
+ var passwordLenght = parseInt(prompt("How many characters would you like your password to contain? ",  "Min:8-Max:129"));
+
+ if (isNaN(passwordLenght)) {
+  alert("Please try again with only numbers.");
+  generatePassword()
+}
+
+  if(passwordLenght < 8 || passwordLenght > 128) {
+    alert("Please try again! (Min:8-Max:128)");
+    generatePassword()
+  }else {
+     var isSymbol = window.confirm("Click OK to confirm including special characters.");
+
+     var isNumeric = window.confirm("Click OK to confirm including numeric characters.");
+
+     var isUpperCase = window.confirm("Click OK to confirm including uppercase characters.");
+
+     var isLowerCase = window.confirm("Click OK to confirm including lowercase characters.");
+  }
+
+
+  if(isSymbol) {
+    var index = Math.floor(Math.random() * symbol.length);
+      possibleChar = symbol[index];
+  }
+
+  if(isNumeric) {
+    var index = Math.floor(Math.random() * numbers.length);
+      numeric = numbers[index];
+  }
+
+  if(isUpperCase) {
+    var index = Math.floor(Math.random() * upperCase.length);
+      upperC = upperCase[index];
+  }
+
+  if(isLowerCase){
+    var index = Math.floor(Math.random() * lowerCase.length);
+    lowC = lowerCase[index];
+  }
+
+
+  for (var i -0; i<passwordLenght-4; i++;) {
+
+  }
+
+  if( isSymbol || isNumeric || isUpperCase || isLowerCase){
+
+
+    console.log(passwordLenght, specialChar, numeric, lowC, upperC, possibleChar);
+  }else {
+    alert("You need to select at least one of symbols, numbers, uppercase or lowercase.")
+  }
+
+
+  return "";
+  }
 
 // Write password to the #password input
 function writePassword() {
@@ -11,71 +96,42 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
- 
-function generatePassword() {
 
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var symbol =  " !\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~";
-  var numbers = "1234567890";
 
-  var howCharacters;
 
-  var passwordLenght;
-  var specialChar;
-  var numeric;
-  var lowC;
-  var upperC;
-  var possibleChar = "";
-  var randomChar = "";
 
-  var maxLength = 8;
-  var maxLength = 128;
-  var password = "";
-  var text;
 
-  // Ask user how many character wants in the password.
-   passwordLenght = parseInt(prompt("How many characters would you like your password to contain? ",  "Min:8-Max:129"))
 
-if (isNaN(passwordLenght)) {
-    alert("Please try again with only numbers.");
-    generatePassword()
-}else if(passwordLenght < 8 || passwordLenght > 128) {
-     alert("Please try again! (Min:8-Max:128)");
-     generatePassword()
-   }
 
-   specialChar = confirm("Click OK to confirm including special characters.")
+//    specialChar = confirm("Click OK to confirm including special characters.")
 
-   if (specialChar == true) {
-    var index = Math.floor(Math.random() * symbol.length);
-    possibleChar = symbol[index];
-   }
+//    if (specialChar == true) {
+//     var index = Math.floor(Math.random() * symbol.length);
+//     possibleChar = symbol[index];
+//    }
 
-   numeric = confirm("Click OK to confirm including numeric characters.")
+//    numeric = confirm("Click OK to confirm including numeric characters.")
 
-   if (numeric == true)  {
-    var index = Math.floor(Math.random() * numbers.length);
-    numeric = numbers[index];
-   }
+//    if (numeric == true)  {
+//     var index = Math.floor(Math.random() * numbers.length);
+//     numeric = numbers[index];
+//    }
 
-   lowC = confirm("Click OK to confirm including lower case characters.")
+//    lowC = confirm("Click OK to confirm including lower case characters.")
 
-   if (lowC == true) {
-    var index = Math.floor(Math.random() * lowerCase.length);
-    lowC = lowerCase[index];
-   }
+//    if (lowC == true) {
+//     var index = Math.floor(Math.random() * lowerCase.length);
+//     lowC = lowerCase[index];
+//    }
    
-   upperC = confirm("Click OK to confirm including upper case characters.")
+//    upperC = confirm("Click OK to confirm including upper case characters.")
 
-   if (upperC == true) {
-    var index = Math.floor(Math.random() * upperCase.length);
-    upperC = upperCase[index];
-   }
+//    if (upperC == true) {
+//     var index = Math.floor(Math.random() * upperCase.length);
+//     upperC = upperCase[index];
+//    }
 
- console.log(passwordLenght, specialChar, numeric, lowC, upperC, possibleChar);
+//  console.log(passwordLenght, specialChar, numeric, lowC, upperC, possibleChar);
 
 
 
@@ -118,6 +174,4 @@ if (isNaN(passwordLenght)) {
 
 // }
 
-  return "";
-}
 
